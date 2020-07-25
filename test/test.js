4f13ae4781af7627bf6f2e220eff89110b9035bd
evanhackett <evanwhackett@gmail.com>
2020-07-24 19:10:30 -0700

    test for filter and find

--- test/test.js
+++ test/test.js
@@ -85,7 +85,113 @@ describe('map', async assert => {
 
 })
 
+describe('filter', async assert => {
+  const iteratee = (val, i, list) => iterateeArguments.push([val, i, list])
+  let iterateeArguments = []
 
+  exercises.filter([], iteratee)
+
+  assert({
+    given: 'empty array',
+    should: 'not call the callback',
+    actual: iterateeArguments,
+    expected: []
+  })
+
+  assert({
+    given: 'empty array',
+    should: 'return an empty array',
+    actual: exercises.filter([], x => true),
+    expected: []
+  })
+
+
+  iterateeArguments = []
+  const list = ['a', 'b', 'c']
+  exercises.filter(list, iteratee)
+
+  assert({
+    given: 'array of values',
+    should: 'loop over all elements, and call the callback passing in the value, index, and array for each element.',
+    actual: iterateeArguments,
+    expected: [
+      ['a', 0, list],
+      ['b', 1, list],
+      ['c', 2, list],
+    ]
+  })
+
+  assert({
+    given: 'array of numbers and an isEven predicate function',
+    should: 'return a new array of all the even numbers',
+    actual: exercises.filter([1,2,3,4], x => x % 2 === 0),
+    expected: [2,4]
+  })
+
+  const xs = [1,2,3,4,5]
+  const result = exercises.filter(xs, x => x < 3)
+
+  assert({
+    given: 'an array and a callback',
+    should: 'return a new array without modifying the original',
+    actual: [xs, result],
+    expected: [[1,2,3,4,5], [1,2]]
+  })
+
+})
+
+describe('find', async assert => {
+  const iteratee = (val, i, list) => {iterateeArguments.push([val, i, list])}
+  let iterateeArguments = []
+
+  exercises.find([], iteratee)
+
+  assert({
+    given: 'empty array',
+    should: 'not call the callback',
+    actual: iterateeArguments,
+    expected: []
+  })
+
+  assert({
+    given: 'empty array',
+    should: 'return undefined',
+    actual: exercises.find([], x => true),
+    expected: undefined
+  })
+
+
+  iterateeArguments = []
+  const list = ['a', 'b', 'c']
+  exercises.find(list, iteratee)
+
+  assert({
+    given: 'array of values',
+    should: 'loop over all elements, and call the callback passing in the value, index, and array for each element.',
+    actual: iterateeArguments,
+    expected: [
+      ['a', 0, list],
+      ['b', 1, list],
+      ['c', 2, list],
+    ]
+  })
+
+  assert({
+    given: 'array of numbers and an isEven predicate function',
+    should: 'return the first number that is even',
+    actual: exercises.find([1,3,5,16,20], x => x % 2 === 0),
+    expected: 16
+  })
+
+   assert({
+    given: 'array of values where none of them match the predicate',
+    should: 'return undefined',
+    actual: exercises.find([1,3,5,16], x => x > 20),
+    expected: undefined
+  })
+
+
+})
 
 
 
